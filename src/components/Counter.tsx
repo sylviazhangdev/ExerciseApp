78ea647b925a8e55d416014e6d6464456b73efa1
SylviaZhang <xinyunz060@gmail.com>
2026-08-27 23:00:05 +1000

    feat: add a Counter Component and Replace Exercise1

---
+++ src/components/Counter.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { View, StyleSheet} from "react-native";
+import ScreenContainer from "./ScreenContainer";
+import { Button, Text } from "react-native-paper";
+
+export default function Counter() {
+
+    /* initial count equals 0, setCount with the value change */
+    const [count, setCount] = useState(0);
+
+    return <View style={styles.container}>
+        <Text variant="headlineMedium">
+            Counter :{count}
+        </Text>
+
+        <Button
+            mode = "contained"
+            onPress={()=> setCount(count+1)}
+        >  
+         INCREMENT
+        </Button>
+
+        <Button
+            mode = "contained"
+            onPress={()=> setCount(count-1)}
+            disabled={count===0}
+        >  
+         DECREMENT
+        </Button>
+
+    </View>
+
+}
+
+const styles = StyleSheet.create({
+    container:{
+        gap: 16,
+    }
+})
--- src/screens/Exercise1Screen.tsx
+++ src/screens/Exercise1Screen.tsx
@@ -1,11 +1,12 @@
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 import ScreenContainer from "../components/ScreenContainer";
+import Counter from "../components/Counter";
 
 export default function Exercise1Screen() {
     return (
         <ScreenContainer>
-            <Text> This is Excercise1!</Text>
+            <Counter/>
         </ScreenContainer>
     );
 }
