74da34db9a3cb610145baf3319a4fe589fbc4ce3
Sylvia Zhang <xinyunz060@gmail.com>
2026-08-28 10:42:09 +1000

    fix: fixed navigator styles

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -17,9 +17,34 @@ export default function BottomTabNavigator() {
             headerStyle: {
                 backgroundColor :theme.colors.primary,
             },
-            // tabBarStyle: {
 
-            // },
+            // tabBarStyle: styles the outer bar container
+            tabBarStyle: {
+                backgroundColor: "#fff",
+                height:64, //Safe Height (esp. for web)
+                borderTopWidth : 1,
+                borderTopColor : theme.colors.outlineVariant,
+
+            },
+
+            // tabBarItemStyle :styles each tab item
+            tabBarItemStyle:{
+                
+            },
+
+            // tabBarLabelStyle : styles label text
+            tabBarLabelStyle:{
+                ...theme.fonts.labelSmall, // Use paper typography styles
+                //Can do Customize style
+                fontSize:12,
+                lineHeight:14, // Try to prevent text clipping (esp. for web)
+                marginTop:2,
+                marginBottom:0,
+            },
+
+            // tabBarIconStyle: styles the icon wapper
+            tabBarIconStyle:{},
+
             headerTintColor: theme.colors.onPrimary,
             tabBarActiveTintColor: theme.colors.primary,
             tabBarInactiveTintColor: theme.colors.outline,
