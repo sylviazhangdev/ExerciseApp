61b5ad979544ab5ecc5ee560d2fb10c889ebdfcb
Sylvia Zhang <xinyunz060@gmail.com>
2026-09-11 12:57:57 +1000

    feat: added persistence of light/dark theme using AsyncStorage

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,9 +1,12 @@
-import { useContext, createContext, PropsWithChildren, useState, useMemo } from "react";
+import { useContext, createContext, PropsWithChildren, useState, useMemo, useEffect } from "react";
 import { useColorScheme } from "react-native";
 import { combinedLightTheme, combinedDarkTheme } from "../theme/theme";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 
-//
+const ASYNC_STORAGE_KEY = "@exercise-app:theme-override";
+
+
 type ThemeContextType = {
     theme: typeof combinedLightTheme,
     overrideTheme: string | null,
@@ -23,9 +26,43 @@ export function ThemeProvider({ children }: PropsWithChildren) {
     // Allow overriding theme
     const [overrideTheme, setOverrideTheme] = useState<string | null>(null);
 
+    // Load saved theme on startup(AsyncStorage)
+     useEffect(() => {
+        loadStoredTheme();
+    }, []);
+
+    const loadStoredTheme = async ()=>{
+        try {
+          const savedTheme = await AsyncStorage.getItem(ASYNC_STORAGE_KEY);
+          
+          if(savedTheme !== null) {
+            setOverrideTheme(JSON.parse(savedTheme));
+          }
+            
+        } catch (error) {
+           console.warn("Failed to load theme preference",error);
+        }
+
+    };
+
+    // Save theme when it is changed(AsyncStorage)
+    useEffect(() => {
+        saveStoredTheme();
+    }, [overrideTheme]);
+
+    //saveStoredTheme
+    const saveStoredTheme = async ()=>{
+        try {
+            await AsyncStorage.setItem(ASYNC_STORAGE_KEY , JSON.stringify(overrideTheme));
+            
+        } catch (error) {
+           console.warn("Failed to save theme preference",error);
+        }
+    };
+
     // Determine theme to use
     const theme = useMemo(() => {
-        
+
         // Check theme overrides
         if (overrideTheme === "light") return combinedLightTheme;
         if (overrideTheme === "dark") return combinedDarkTheme;
